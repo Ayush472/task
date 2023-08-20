8dc5a32c6935300204b6c6d5b1463f968a8c4c3d
dev <dev.dpparikh@gmail.com>
2023-08-20 17:52:12 +0530

    Added customer name
    Added customer name

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -2,8 +2,10 @@ const { faker } = require("@faker-js/faker");
 const { prisma } = require("./client");
 
 async function main() {
+  await prisma.transaction.deleteMany({})
   const products =  Array.from(Array(5).keys()).map(i=>faker.commerce.productName())
   const transactions = Array.from(Array(100).keys()).map((i) => ({
+    customer_name : faker.person.fullName(),
     product_purchased: faker.helpers.arrayElement(products),
     quantity: faker.number.int({ max: 10 }),
     sales: faker.number.float({ max: 100 }),
